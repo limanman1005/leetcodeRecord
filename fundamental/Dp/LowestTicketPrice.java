895947650115a8cb63b72384117bb523e66f2ac6
liyh <liyh@sucsoft.com>
2020-05-06 22:32:20 +0800

    两道dfs，一道每日一题，一道回溯

---
+++ fundamental/Dp/LowestTicketPrice.java
@@ -0,0 +1,51 @@
+package Dp;
+
+/**
+ * ClassName: LowestTicketPrice
+ * Description:
+ * date: 2020/5/6 11:09
+ *
+ * @author liyh
+ */
+public class LowestTicketPrice {
+}
+
+class Solution {
+    /**
+     * dp算法的思维方式没有掌握，暂时留个坑，以后再说
+     * @param days
+     * @param costs
+     * @return
+     */
+    public int mincostTickets(int[] days, int[] costs) {
+        int len = days.length;
+        int n = days[len-1];
+        int[] dp = new int[n+1];
+        int a, b, c;
+        for(int i = 0; i < len; ++i){
+            dp[days[i]] = -1;
+        }
+        for(int i = 1; i <= n; ++i){
+            if(dp[i] == 0){
+                dp[i] = dp[i-1];
+            }
+            else{
+                a = dp[i-1] + costs[0];
+                if(i-7 >= 0){
+                    b = dp[i-7] + costs[1];
+                }
+                else{
+                    b = costs[1];
+                }
+                if(i-30 >= 0){
+                    c = dp[i-30] + costs[2];
+                }
+                else{
+                    c = costs[2];
+                }
+                dp[i] = Math.min(a, Math.min(b, c));
+            }
+        }
+        return dp[n];
+    }
+}
---
+++ fundamental/backTrack/Lc784.java
@@ -0,0 +1,67 @@
+package backTrack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ClassName: Lc784
+ * Description:
+ * date: 2020/5/6 11:52
+ *
+ * @author liyh
+ */
+public class Lc784 {
+    public static void main(String[] args) {
+
+    }
+}
+class Solution {
+    public List<String> letterCasePermutation(String S) {
+        List<String> ans = new ArrayList<>();
+        if(S.length() == 0){
+            return ans;
+        }
+        char[] strChar = S.toCharArray();
+        helper(strChar, 0, S.length(), ans);
+        return ans;
+    }
+    public void helper(char[] str, int cur, int end, List<String> ans){
+        if(cur == end){
+            ans.add(new String(str));
+            return;
+        }
+        helper(str, cur + 1, end, ans);
+        if(Character.isLetter(str[cur])){
+            str[cur] = (char) (str[cur] ^ 32);
+            helper(str, cur + 1, end, ans);
+        }
+    }
+
+
+    public List<String> letterCasePermutation2(String S) {
+        List<String> ans = new ArrayList<>();
+        if(S.isEmpty()){
+            return ans;
+        }
+        dfs(S, 0, ans, new StringBuilder());
+        return ans;
+    }
+    public void dfs(String s, int cur, List<String> ans, StringBuilder sb){
+        if(s.length() == cur){
+            ans.add(sb.toString());
+            return;
+        }
+        if(Character.isDigit(s.charAt(cur))){
+            sb.append(s.charAt(cur));
+            dfs(s, cur + 1, ans, sb);
+            sb.deleteCharAt(sb.length() - 1);
+        }else{
+            sb.append(s.charAt(cur));
+            dfs(s, cur + 1, ans, sb);
+            sb.deleteCharAt(sb.length() - 1);
+            sb.append((char)(s.charAt(cur) ^ 32));
+            dfs(s, cur + 1, ans, sb);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+}
