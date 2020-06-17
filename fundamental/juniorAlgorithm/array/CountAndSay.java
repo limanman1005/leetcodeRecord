03aca048d5712d6d048c64a33bb00a8e465a409f
liyh <liyh@sucsoft.com>
2020-06-17 10:05:36 +0800

    每日一题和初级算法的字符串题

--- fundamental/juniorAlgorithm/array/CountAndSay.java
+++ fundamental/juniorAlgorithm/array/CountAndSay.java
@@ -3,6 +3,7 @@ package juniorAlgorithm.array;
 /**
  * ClassName: CountAndSay
  * Description: 这个是字符串那一节的题，不小心放错了。
+ * 这道题可以用递归也可以用循环，递归好写一点
  * date: 2020/6/3 9:47
  *
  * @author liyh
@@ -10,7 +11,7 @@ package juniorAlgorithm.array;
 public class CountAndSay {
     public static void main(String[] args) {
         Solution38 solution38 = new Solution38();
-        String s = solution38.countAndSay(4);
+        String s = solution38.countAndSay2(4);
         System.out.println(s);
     }
 }
@@ -35,6 +36,34 @@ class Solution38 {
         if(left != right){
             int count = right - left;
             sb.append(count).append(prev.charAt(left));
+        }
+        return sb.toString();
+    }
+
+
+
+    public String countAndSay2(int n) {
+        if(n <= 0){
+            return "";
+        }
+        if(n == 1){
+            return "1";
+        }
+        String prev = countAndSay2(n - 1);
+        int left = 0;
+        int right = 1;
+        char[] chars = prev.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for(; right < chars.length; ++right){
+            if(chars[left] != chars[right]){
+                sb.append(right - left).append(chars[left]);
+                left = right;
+            }
+        }
+        //这个条件有点秀啊，绕过了下标越界的判断
+        //或者使用一个count也挺好
+        if(left != right){
+            sb.append(right - left).append(chars[left]);
         }
         return sb.toString();
     }
