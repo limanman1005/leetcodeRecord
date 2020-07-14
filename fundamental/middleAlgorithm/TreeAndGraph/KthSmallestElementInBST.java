e5015e3686f5a708c425b8918da2de7f45f6baf1
liyh <liyh@sucsoft.com>
2020-07-14 20:23:11 +0800

    中级算法里面的树和排序搜索的题写了两道

---
+++ fundamental/middleAlgorithm/TreeAndGraph/KthSmallestElementInBST.java
@@ -0,0 +1,72 @@
+package middleAlgorithm.TreeAndGraph;
+
+import Dfs.TreeNode;
+
+import java.util.Stack;
+
+/**
+ * ClassName: KthSmallestElementInBST
+ * Description:给一个二叉搜索树，找到第k个大小的数
+ * date: 2020/7/14 19:57
+ *
+ * @author liyh
+ */
+public class KthSmallestElementInBST {
+}
+class Solution230 {
+    private int ans;
+    private int num;
+
+    /**
+     * 递归解法
+     * @param root
+     * @param k
+     * @return
+     */
+    public int kthSmallest(TreeNode root, int k) {
+        dfs(root, k);
+        return ans;
+    }
+    private void dfs(TreeNode root, int k){
+        if(root == null){
+            return ;
+        }
+        dfs(root.left, k);
+        num++;
+        if(num == k){
+            ans = root.val;
+            return;
+        }
+        dfs(root.right, k);
+    }
+
+
+    /**
+     * 迭代解法
+     * @param root
+     * @param k
+     * @return
+     */
+    public int kthSmallest2(TreeNode root, int k) {
+        Stack<TreeNode> stack = new Stack<>();
+        int num = 0;
+        int ans = -1;
+        TreeNode cur = root;
+        while(cur != null || !stack.isEmpty()){
+            while(cur != null){
+                stack.push(cur);
+                cur = cur.left;
+            }
+            if(!stack.isEmpty()){
+                cur = stack.pop();
+                num++;
+                if(num == k){
+                    ans = cur.val;
+                    break;
+                }
+                cur = cur.right;
+            }
+        }
+        return ans;
+    }
+}
