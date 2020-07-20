09672a3112d92c63ccffe1fce8385ece85ca6ff8
liyh <liyh@sucsoft.com>
2020-07-20 20:05:46 +0800

    补下上周的每日一题

---
+++ fundamental/JulyPractice/BurstBalloons.java
@@ -0,0 +1,13 @@
+package JulyPractice;
+
+/**
+ * ClassName: BurstBalloons
+ * Description: 戳气球，每一个气球上有一个数字。
+ * 戳一个气球之后可以得到气球本身上的数字和左右两边气球上的数字的乘积。
+ * 求气球全部戳破之后可以得到的最大硬币数
+ * date: 2020/7/20 10:26
+ *
+ * @author liyh
+ */
+public class BurstBalloons {
+}
